77d780314d85369c7a66af3d12c6c3c753102430
Elena-Kravtsova <kravtsovaeyu@gmail.com>
2023-11-14 14:49:55 +0200

    Initial commit

---
+++ src/test/java/employees/spring/EmployeesHometaskExceptionsHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package employees.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes= {GlobalExceptionHandler.class})
+class EmployeesHometaskExceptionsHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
